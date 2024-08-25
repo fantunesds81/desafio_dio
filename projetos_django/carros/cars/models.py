16bcebfb3a5edf5130612e059bb916348f33cf14
fantunesds81 <fantunesds@gmail.com>
2024-08-24 22:55:41 -0300

    ...........

--- projetos_django/carros/cars/models.py
+++ projetos_django/carros/cars/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+# modelos de marcas
+
+class Brand(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
+
+# modelo de carro
+
+class Car(models.Model):
+    model = models.CharField(max_length=255, blank=False, null=False)
+    brand = models.ForeignKey(Brand, on_delete=PROTECT, related_name='car_brand')
+    factory_year = models.IntegerField(blank=True, null=True)
+    model_year = models.IntegerField(blank=True, null=True)
+    plate = models.CharField(max_length=10, blank=True, null=True)
+    value = models.DecimalField(max_digits=12, decimal_places=2, blank=False, null=False)
+    photo = models.ImageField(upload_to='cars/', blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.model
+    
+# modelo de inventario do nosso signal
+class CarInventory(models.Model):
+    cars_count = models.IntegerField()
+    cars_value = models.FloatField()
+    created_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.cars_count} - {self.cars_value}'
+
