f7df98e4711c89fa8f6d279feb5b7f5ace8e0f71
fantunesds81 <fantunesds@gmail.com>
2024-09-01 13:22:31 -0300

    dio
    dio

--- chatgpt_node_react/src/app.js
+++ chatgpt_node_react/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const cors = require("cors")
+const routes = require("./routes/routes")
 
 require("dotenv").config()
 
@@ -7,6 +8,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(routes)
 
 module.exports = app
 
