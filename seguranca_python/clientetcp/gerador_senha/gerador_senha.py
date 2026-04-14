d025af547518360dc6b377870de3d50afd76c462
fantunesds81 <fantunesds@gmail.com>
2026-04-13 23:45:42 -0300

    oh

--- seguranca_python/clientetcp/gerador_senha/gerador_senha.py
+++
@@ -1,8 +0,0 @@
-import random
-import string
-
-tamanho = 16
-chars =string.ascii_letters + string.digits + '!@#$%&*()-+='
-rnd = random.SystemRandom()
-
-print(''.join(rnd.choice(chars) for i in range(tamanho)))
--- seguranca_python/wordlist/wordlist.py
+++
@@ -1,8 +0,0 @@
-import itertools
-
-string = input("String a ser permutada: ")
-
-resuldado = itertools.permutations(string, len(string))
-
-for i in resuldado:
-    print(''.join(i))
